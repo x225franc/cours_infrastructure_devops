8f110b0430a2bdd267012d91079aedf175666b4c
Alpha Malick DIAWARA <102668503+x225franc@users.noreply.github.com>
2026-03-06 00:48:08 +0100

    Exercice_1

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -156,3 +156,31 @@ SPECTACULAR_SETTINGS = {
         {"name": "Quotidien", "description": "Donnees journalieres agregees"},
     ],
 }
+
+LOGGING = {
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'info.log',
+            'formatter': 'verbose',  
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
